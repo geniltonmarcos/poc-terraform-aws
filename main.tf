18a281e096ef9d77df26e36f19497d9c7d4c4d16
Genilton Marcos <genilton.marcos@ciandt.com>
2023-08-15 16:19:25 -0300

    Commit inicial

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 
+## BUCKET
 resource "aws_s3_bucket" "bucket_data_processed" {
-  bucket = "bucket-processed"  # Altere para um nome único
+  bucket = "bucket-glue-processed" 
   force_destroy = true
   acl    = "private"
 
@@ -15,10 +16,9 @@ resource "aws_s3_bucket" "bucket_data_processed" {
 
 #Bucket scripts
 resource "aws_s3_bucket" "bucket_script" {
-  bucket = "bucket-script" 
+  bucket = "bucket-glue-script" 
   force_destroy = true
-  acl    = "private"
-
+ 
   tags = {
     Name = "TerraForm S3 Glue"
   }
@@ -31,7 +31,7 @@ variable "files_script_to_upload" {
 resource "aws_s3_bucket_object" "upload_objects_script" {
  count = length(var.files_script_to_upload)
 
-  bucket = aws_s3_bucket.bucket_data_unprocessed.id
+  bucket = aws_s3_bucket.bucket_script.id
   key    = var.files_script_to_upload[count.index]
   source = "script/${var.files_script_to_upload[count.index]}"
 }
@@ -59,49 +59,133 @@ resource "aws_s3_bucket_object" "upload_objects_data" {
   source = "files/${var.files_data_to_upload[count.index]}"
 }
 
-## creating aws glue
-resource "aws_glue_catalog_database" "catalog_database_players_full" {
-  name = "catalog_database_players_full"
-}
-
-resource "aws_glue_catalog_table" "table_players_full" {
-  name     = "table_players_full"
-  database = aws_glue_catalog_database.catalog_database_players_full.name
-
-  table_type = "EXTERNAL_TABLE"
-
-  storage_descriptor {
-    location = "s3://bucket-processed/temp/"
-  }
-
-  # partition_keys = [
-  #   {
-  #     name = "partition_column_name",
-  #     type = "string",
-  #   },
-  # ]
-}
-
-## recupera o aws account Id
-data "aws_caller_identity" "current" {}
+#### GLUE
 locals {
-    account_id = data.aws_caller_identity.current.account_id
+  full_name = "${var.organization}${length(var.organization) > 0 ? "-" : ""}${var.environment}-${var.name}"
+  tags      = merge({
+    Terraform   = "True"
+    Environment = var.environment
+  }, var.tags)
 }
 
-resource "aws_glue_job" "job_history_players" {
-  name     = "job_process_history_players"
-  role_arn = "arn:aws:iam::${local.account_id}:role/AWSGlueServiceRole-analytics-glue"
+resource "aws_glue_job" "this" {
+  name                   = local.full_name
+  role_arn               = var.create_role ? join("", aws_iam_role.role.*.arn) : var.role_arn
+  connections            = var.connections
+  description            = var.description
+  glue_version           = var.glue_version
+  max_retries            = var.max_retries
+  timeout                = var.timeout
+  security_configuration = var.create_security_configuration ? join("", aws_glue_security_configuration.sec_cfg.*.id) : var.security_configuration
+  worker_type            = var.worker_type
+  number_of_workers      = var.number_of_workers
+  tags                   = local.tags
 
   command {
     name            = "glueetl"
-    script_location = "s3://bucket-script/process_player.py"
+    script_location = var.script_location
+    python_version  = var.python_version
   }
 
   default_arguments = {
-    "--job-language" = "python"
+    "--job_process_history_players"           = local.full_name
+    "--job-language"                          = var.job_language
+    "--class"                                 = var.class
+    "--extra-py-files"                        = length(var.extra_py_files) > 0 ? join(",", var.extra_py_files) : null
+    "--extra-jars"                            = length(var.extra_jars) > 0 ? join(",", var.extra_jars) : null
+    "--user-jars-first"                       = var.user_jars_first
+    "--use-postgres-driver"                   = var.use_postgres_driver
+    "--extra-files"                           = length(var.extra_files) > 0 ? join(",", var.extra_files) : null
+    "--job-bookmark-option"                   = var.job_bookmark_option
+    "--TempDir"                               = var.temp_dir
+    "--enable-s3-parquet-optimized-committer" = var.enable_s3_parquet_optimized_committer
+    "--enable-rename-algorithm-v2"            = var.enable_rename_algorithm_v2
+    "--enable-glue-datacatalog"               = var.enable_glue_datacatalog ? "" : null
+    "--enable-metrics"                        = var.enable_metrics ? "" : null
+    "--enable-continuous-cloudwatch-log"      = var.enable_continuous_cloudwatch_log
+    "--enable-continuous-log-filter"          = var.enable_continuous_log_filter
+    "--continuous-log-logGroup"               = join("", aws_cloudwatch_log_group.log_group.*.name)
+    "--continuous-log-logStreamPrefix"        = var.continuous_log_stream_prefix
+    "--continuous-log-conversionPattern"      = var.continuous_log_conversion_pattern
+    "--enable-spark-ui"                       = var.enable_spark_ui
+    "--spark-event-logs-path"                 = var.spark_event_logs_path
+    "--additional-python-modules"             = length(var.additional_python_modules) > 0 ? join(",", var.additional_python_modules) : null
   }
 
   execution_property {
-    max_concurrent_runs = 1
+    max_concurrent_runs = var.max_concurrent_runs
   }
+
+  dynamic "notification_property" {
+    for_each = var.notify_delay_after == null ? [] : [1]
+
+    content {
+      notify_delay_after = var.notify_delay_after
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = "/aws-glue/jobs/${local.full_name}"
+  retention_in_days = var.log_group_retention_in_days
+  tags              = var.tags
+}
+
+resource "aws_glue_security_configuration" "sec_cfg" {
+  count = var.create_security_configuration ? 1 : 0
+  name  = "${local.full_name}-sec-config"
+
+  encryption_configuration {
+    dynamic "cloudwatch_encryption" {
+      for_each = [var.security_configuration_cloudwatch_encryption]
+
+      content {
+        cloudwatch_encryption_mode = cloudwatch_encryption.value.cloudwatch_encryption_mode
+        kms_key_arn                = cloudwatch_encryption.value.kms_key_arn
+      }
+    }
+
+    dynamic "job_bookmarks_encryption" {
+      for_each = [var.security_configuration_job_bookmarks_encryption]
+
+      content {
+        job_bookmarks_encryption_mode = job_bookmarks_encryption.value.job_bookmarks_encryption_mode
+        kms_key_arn                   = job_bookmarks_encryption.value.kms_key_arn
+      }
+    }
+
+    dynamic "s3_encryption" {
+      for_each = [var.security_configuration_s3_encryption]
+
+      content {
+        s3_encryption_mode = s3_encryption.value.s3_encryption_mode
+        kms_key_arn        = s3_encryption.value.kms_key_arn
+      }
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  count = var.create_role ? 1 : 0
+  name  = "${local.full_name}-role"
+  tags  = local.tags
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = [
+          "sts:AssumeRole"
+        ]
+        Principal = {
+          "Service" = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+  ]
 }
--- script/process_player.py
+++ script/process_player.py
@@ -1,29 +1,70 @@
 import sys
-from awsglue.transforms import Join
-from awsglue.context import GlueContext
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from pyspark.sql import SparkSession
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
-# Inicializa o contexto do Glue
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+## @type: DataSource
+## @args: [format_options = {"quoteChar":"\"","escaper":"","withHeader":True,"optimizePerformance":False,"separator":",","multiline":False}, connection_type = "s3", format = "csv", connection_options = {"paths": ["s3://bucket-unprocessed/addresses.csv"], "recurse":True}, transformation_ctx = "DataSource0"]
+## @return: DataSource0
+## @inputs: []
+DataSource0 = glueContext.create_dynamic_frame.from_options(format_options = {"quoteChar":"\"","escaper":"","withHeader":True,"optimizePerformance":False,"separator":",","multiline":False}, connection_type = "s3", format = "csv", connection_options = {"paths": ["s3://bucket-unprocessed/addresses.csv"], "recurse":True}, transformation_ctx = "DataSource0")
+## @type: DataSource
+## @args: [format_options = {"quoteChar":"\"","escaper":"","withHeader":True,"optimizePerformance":False,"separator":","}, connection_type = "s3", format = "csv", connection_options = {"paths": ["s3://bucket-unprocessed/mlb_players.csv"], "recurse":True}, transformation_ctx = "DataSource1"]
+## @return: DataSource1
+## @inputs: []
+DataSource1 = glueContext.create_dynamic_frame.from_options(format_options = {"quoteChar":"\"","escaper":"","withHeader":True,"optimizePerformance":False,"separator":","}, connection_type = "s3", format = "csv", connection_options = {"paths": ["s3://bucket-unprocessed/mlb_players.csv"], "recurse":True}, transformation_ctx = "DataSource1")
+## @type: Join
+## @args: [columnConditions = ["="], joinType = left, keys2 = ["PlayerId"], keys1 = ["PlayerId"], transformation_ctx = "Transform0"]
+## @return: Transform0
+## @inputs: [frame1 = DataSource1, frame2 = DataSource0]
+DataSource1DF = DataSource1.toDF()
+DataSource0DF = DataSource0.toDF()
+Transform0 = DynamicFrame.fromDF(DataSource1DF.join(DataSource0DF, (DataSource1DF['PlayerId'] == DataSource0DF['PlayerId']), "left"), glueContext, "Transform0")
+## @type: DataSink
+## @args: [format_options = {"compression": "snappy"}, connection_type = "s3", format = "glueparquet", connection_options = {"path": "s3://bucket-glue-processed", "partitionKeys": []}, transformation_ctx = "DataSink0"]
+## @return: DataSink0
+## @inputs: [frame = Transform0]
+DataSink0 = glueContext.write_dynamic_frame.from_options(frame = Transform0, format_options = {"compression": "snappy"}, connection_type = "s3", format = "glueparquet", connection_options = {"path": "s3://bucket-glue-processed", "partitionKeys": []}, transformation_ctx = "DataSink0")
+job.commit()
 
-# Obtém as opções passadas para o script
-args = getResolvedOptions(sys.argv, ['job_history_players'])
 
-# Cria um dataframe a partir dos arquivos CSV
-df1 = spark.read.option("header", "true").csv("s3://bucket-unprocessed/mbl_players.csv")
-df2 = spark.read.option("header", "true").csv("s3://bucket-unprocessed/addresses.csv")
+# import sys
+# from awsglue.transforms import Join
+# from awsglue.context import GlueContext
+# from awsglue.utils import getResolvedOptions
+# from pyspark.context import SparkContext
+# from pyspark.sql import SparkSession
 
-# Define as colunas para o join
-join_keys = ["PlayerId"]
+# # Inicializa o contexto do Glue
+# sc = SparkContext()
+# glueContext = GlueContext(sc)
+# spark = glueContext.spark_session
 
-# Realiza o Left Join
-joined_df = df1.join(df2, join_keys, "left")
+# # Obtém as opções passadas para o script
+# args = getResolvedOptions(sys.argv, ['job_process_history_players'])
 
-# Escreve o resultado em um arquivo CSV no bucket de destino
-joined_df.write.option("header", "true").csv("s3://bucket-processed/results.csv")
+# # Cria um dataframe a partir dos arquivos CSV
+# df1 = spark.read.option("header", "true").csv("s3://bucket-unprocessed/mlb_players.csv")
+# df2 = spark.read.option("header", "true").csv("s3://bucket-unprocessed/addresses.csv")
 
-# Fim do script
+# # Define as colunas para o join
+# join_keys = ["PlayerId"]
+
+# # Realiza o Left Join
+# joined_df = df1.join(df2, join_keys, "left")
+
+# # Escreve o resultado em um arquivo CSV no bucket de destino
+# joined_df.write.option("header", "true").csv("s3://bucket-glue-processed/results.csv")
+
+# # Fim do script
